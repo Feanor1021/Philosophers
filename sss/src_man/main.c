ff0e4e9bb8e76c68955b901706f50bd5c2bd44d2
Furkan Yardımcı <92301220+Feanor1021@users.noreply.github.com>
2022-10-16 02:03:40 +0300

    Add files via upload

---
+++ sss/src_man/main.c
@@ -0,0 +1,14 @@
+#include  "philo.h"
+
+int main(int argc, char **argv)
+{
+    t_arg rules;
+
+    if(argc != 5 && argc != 6)
+        return (error_message("Wrong amount of arguments!!!"));
+    if(set_rules(&rules,argv,argc))
+        return (error_message("\nSomething happend at initilazion phase..."));
+    if(handle_resources(&rules))
+        return (error_message("\nSomething happened at process phase...."));
+    return 0;
+}
